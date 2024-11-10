4205259d2a6fdc814b5733e380fa1c881f2ab2e1
SirAlienTheGreat <allen.macfarland@gmail.com>
2024-11-10 11:43:51 -0700

    base mobile support

--- titlebar.php
+++ titlebar.php
@@ -1,6 +1,24 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <style>
+    @media all and (min-width:655px) {
+        #main_titlebar{
+            display: flex;
+        }
+        #mobile_titlebar{
+            display: none;
+        }
+    }
+    @media all and (max-width:654px) {
+        #main_titlebar{
+            display: none;
+        }
+        #mobile_titlebar{
+            display: block;
+            position: fixed;
+        }
+    }
     #main_titlebar {
-        display:flex;
         gap:15px;
         background-color: rgb(17, 48, 0);
         overflow:hidden;
@@ -28,6 +46,7 @@
         transform: skewX(15deg);
         display: inline-block;
     }
+
 </style>
 <div id="main_titlebar">
     <a href="/index.php" class="titlebar_item">Home</a>
@@ -37,6 +56,48 @@
     <a href="/tv.php" class="titlebar_item">TV production</a>
     <a href="/play-game.php" class="titlebar_item">Play the game!</a>
 
+</div>
+
+<link rel="stylesheet" href="hamburger-menu.css">
+<div id=mobile_titlebar>
+    <div id="menuToggle">
+        <!--
+        A fake / hidden checkbox is used as click reciever,
+        so you can use the :checked selector on it.
+        -->
+        <input type="checkbox" id="menuCheckbox" />
+
+        <!--
+        Some spans to act as a hamburger.
+
+        They are acting like a real hamburger,
+        not that McDonalds stuff.
+        -->
+        <span></span>
+        <span></span>
+        <span></span>
+
+        <!--
+        Too bad the menu has to be inside of the button
+        but hey, it's pure CSS magic.
+        -->
+        <ul id="menu">
+          <!--
+          We can use a label here to close upon click (when doing same page navigation), this
+          does require a slight bit of JS.
+          -->
+          <li><a href="/index.php" class="titlebar_item">Home</a></li>
+          <!--<a href="/capstone-proposal.php" class="titlebar_item">Capstone proposal</a>-->
+          <li><a href="/rust-game.php" class="titlebar_item">rust-game</a></li>
+          <li><a href="/classwork.php" class="titlebar_item">Classwork</a></li>
+          <li><a href="/tv.php" class="titlebar_item">TV production</a></li>
+          <li><a href="/play-game.php" class="titlebar_item">Play the game!</a></li>
+
+
+
+
+        </ul>
+      </div>
 </div>
 
 <link rel="stylesheet" type="text/css" href="styles.css"/>
